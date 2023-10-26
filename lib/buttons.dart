0db7dd9cbe9fb4a2228873d17b69b633e9ca035c
Mihir Bhadange <83822717+stellarboymihir@users.noreply.github.com>
2023-10-26 13:23:53 +0530

    Till UI

---
+++ lib/buttons.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final color;
+  final textColor;
+  final String buttonText;
+
+  const MyButton(
+      {super.key, this.color, this.textColor, required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12.0),
+        child: Container(
+          color: color,
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(color: textColor),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
